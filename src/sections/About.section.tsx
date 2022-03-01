10a2f43ccecadac33ff715f01a47496ef164a7ef
Nhu Tu <nhu.tu@publishone.nl>
2022-03-01 14:15:11 +0700

    structure contact

--- src/sections/About.section.tsx
+++ src/sections/About.section.tsx
@@ -1,8 +1,10 @@
 import {
-  AcademicCapIcon,
+  AtSymbolIcon,
   BookmarkIcon,
-  ChartBarIcon,
+  BriefcaseIcon,
+  PhoneIcon,
   TranslateIcon,
+  UserCircleIcon,
 } from "@heroicons/react/outline";
 import React from "react";
 import Intro from "../components/Intro.component";
@@ -21,7 +23,7 @@ const About: React.FC = () => {
         <Intro
           name="Companies"
           description="4 companies worked"
-          Icon={TranslateIcon}
+          Icon={BriefcaseIcon}
         />
         <Intro
           name="Language"
@@ -31,7 +33,38 @@ const About: React.FC = () => {
       </div>
       <div className="flex items-center justify-center gap-3">
         <img src={avt} alt="my-avatar" className="rounded-xl flex-1" />
-        <div className="intro"></div>
+        <div className="intro">
+          <p>
+            As Frontend developer with more than 4 years experience working with
+            HTML5, CSS3, Javascript. I worked framework is ReactJS, Angular 2+.
+            Expertise in developing UI/UX from design and delivery quality work.
+            I'm keen on learning new technical frontend, embracing challenges to
+            better
+          </p>
+          <div className="contact-info">
+            <div>
+              <span>
+                <UserCircleIcon className="" /> Name
+              </span>
+              <span>:</span>
+              <span>Nhu Tu</span>
+            </div>
+            <div>
+              <span>
+                <PhoneIcon className="" /> Phone
+              </span>
+              <span>:</span>
+              <span>0906 772 584</span>
+            </div>
+            <div>
+              <span>
+                <AtSymbolIcon className="" /> E-mail
+              </span>
+              <span>:</span>
+              <span>tuainhu160296@gmail.com</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
