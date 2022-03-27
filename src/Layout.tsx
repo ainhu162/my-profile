5c28f0b9402d31cab4328d6841bc5ec783fd29fb
Nhu. Tu Ai <nhuta@vng.com.vn>
2022-03-27 17:33:28 +0700

    adjust info

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from 'react';
 
 const Layout: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="dark:bg-slate-900">{children}</div>;
+  return <div className="dark:bg-slate-900 pb-5">{children}</div>;
 };
 
 export default Layout;
--- src/sections/Skills.section.tsx
+++ src/sections/Skills.section.tsx
@@ -165,14 +165,17 @@ const Skills: React.FC = () => {
           </ul>
         </div>
       </div>
-
       <p className="text-2xl dark:text-indigo-300 text-slate-900 mt-5">
         Experiences
       </p>
       <ul className="flex flex-col experience-list">
         <li className="text-right w-1/2 p-3">
-          <p className="time dark:text-violet-300">April 2021 - Now</p>
-          <p className="company uppercase dark:text-indigo-100">IDea logic</p>
+          <p className="time dark:text-violet-300 text-gray-400">
+            April 2021 - Now
+          </p>
+          <p className="company text-teal-600 font-semibold uppercase dark:text-indigo-100">
+            IDea logic
+          </p>
           <div className="dark:text-stone-500 italic">
             <p className="indent-2">
               - Maintain source code and Enhance feature
@@ -184,42 +187,62 @@ const Skills: React.FC = () => {
           </div>
         </li>
         <li className="ml-auto w-1/2 p-3">
-          <p className="time dark:text-violet-300">April 2021 - Now</p>
-          <p className="company uppercase dark:text-indigo-100">IDea logic</p>
+          <p className="time dark:text-violet-300 text-gray-400">
+            Jun 2020 - April 2021
+          </p>
+          <p className="company text-teal-600 font-semibold uppercase dark:text-indigo-100">
+            S3 CORP
+          </p>
           <div className="dark:text-stone-500 italic">
+            <p className="indent-2">- Implement almost the UI of the website</p>
             <p className="indent-2">
-              - Maintain source code and Enhance feature
+              - Communicate with SP team to collect requirements
             </p>
             <p className="indent-2">
-              - Based on Zeplin design to build the applications UI
+              - Based on Figma design to build the applications UI
             </p>
-            <p className="indent-2">- Develop the website using Angular 11</p>
+            <p className="indent-2">
+              - Develop the website using ReactJS( React Hook ) and Auth0 to
+              manage user login
+            </p>
+            <p className="indent-2">- Suggest UX/UI for PO</p>
           </div>
         </li>
+
         <li className="text-right w-1/2 p-3">
-          <p className="time dark:text-violet-300">April 2021 - Now</p>
-          <p className="company uppercase dark:text-indigo-100">IDea logic</p>
+          <p className="time dark:text-violet-300 text-gray-400">
+            Jan 2019 - May 2020
+          </p>
+          <p className="company text-teal-600 font-semibold uppercase dark:text-indigo-100">
+            KMS TECHNOLOGY
+          </p>
           <div className="dark:text-stone-500 italic">
             <p className="indent-2">
-              - Maintain source code and Enhance feature
+              - Worked with the design team to build the applications UI
             </p>
             <p className="indent-2">
-              - Based on Zeplin design to build the applications UI
+              - Developed a web application using ReactJS(Typescript)/ NextJS/
+              Redux
             </p>
-            <p className="indent-2">- Develop the website using Angular 11</p>
+            <p className="indent-2">- Review code for other team members</p>
           </div>
         </li>
         <li className="ml-auto w-1/2 p-3">
-          <p className="time dark:text-violet-300">April 2021 - Now</p>
-          <p className="company uppercase dark:text-indigo-100">IDea logic</p>
+          <p className="time dark:text-violet-300 text-gray-400">
+            Aug 2017 - May 2018
+          </p>
+          <p className="company text-teal-600 font-semibold uppercase dark:text-indigo-100">
+            VNG CORP
+          </p>
           <div className="dark:text-stone-500 italic">
+            <p className="indent-2">- Convert PSD to HTML5, CSS3, JQuery</p>
+            <p className="indent-2">- Create news pages for VNG's Game</p>
             <p className="indent-2">
-              - Maintain source code and Enhance feature
+              - Maintain and Develop tools manage banner, article
             </p>
             <p className="indent-2">
-              - Based on Zeplin design to build the applications UI
+              - Create webpages for users to interact vote, share, social login{' '}
             </p>
-            <p className="indent-2">- Develop the website using Angular 11</p>
           </div>
         </li>
       </ul>
